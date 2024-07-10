3007832e1a3f6d9431fc187cc23c7a4aeed9d7ee
Artem Parashchuk <a.p.paraschuk@gmail.com>
2024-07-10 22:20:41 +0300

    initial styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
-import './App.css'
+import { useDispatch } from "react-redux";
+import s from "./App.module.css";
+import { Footer } from "./components/Footer/Footer";
+import { Header } from "./components/Header/Header";
+import { Layout } from "./components/Layout/Layout";
+import { Sidebar } from "./components/Sidebar/Sidebar";
+import { useEffect } from "react";
+import { getCategoriesThunk } from "./redux/categories/operations";
 
 function App() {
+  const dispatch = useDispatch()
 
+  useEffect(() => {
+    dispatch(getCategoriesThunk())
+  }, [dispatch])
   return (
-    <>
+    <div className={s.app}>
+      <Header />
 
-    </>
-  )
+      <div className={s.container}>
+        <Sidebar />
+        <Layout />
+      </div>
+
+      <Footer/>
+    </div>
+  );
 }
 
-export default App
+export default App;
